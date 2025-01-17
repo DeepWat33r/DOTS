1fd80898c838e68ec96368016855b0d38ed207d7
Denys <denis.gidzun@gmail.com>
2025-01-17 20:19:43 +0200

    Unit Visual Selecting

--- Assets/Scripts/Authoring/SelectedAuthoring.cs
+++ Assets/Scripts/Authoring/SelectedAuthoring.cs
@@ -5,13 +5,18 @@ namespace Authoring
 {
     public class SelectedAuthoring : MonoBehaviour
     {
-        
+        public GameObject visualGameObject;
+        public float showScale;
         public class Baker : Baker<SelectedAuthoring>
         {
             public override void Bake(SelectedAuthoring authoring)
             {
                 Entity entity = GetEntity(TransformUsageFlags.Dynamic);
-                AddComponent(entity, new Selected());
+                AddComponent(entity, new Selected()
+                {
+                    visualEntity = GetEntity(authoring.visualGameObject, TransformUsageFlags.Dynamic),
+                    showScale = authoring.showScale
+                });
                 SetComponentEnabled<Selected>(entity, false);
             }
         }
@@ -21,7 +26,8 @@ namespace Authoring
     
     public struct Selected : IComponentData, IEnableableComponent
     {
-    
+        public Entity visualEntity;
+        public float showScale;
     }
 }
 
